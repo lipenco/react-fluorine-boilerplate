829fce5361619dfb2dbd401d5fee7e0bb70642f7
Phil Plückthun <phil@plckthn.me>
2016-05-18 23:22:11 +0100

    Initial Commit

---
+++ server.js
@@ -0,0 +1,34 @@
+/* eslint-disable */
+
+var path = require('path')
+var webpack = require('webpack')
+var webpackDevServer = require('webpack-dev-server')
+var config = require('./webpack.config')
+
+config.watch = true
+
+var port = process.env.PORT || 3000
+var compiler = webpack(config)
+var server = new webpackDevServer(compiler, {
+  contentBase: 'www',
+  historyApiFallback: true,
+  noInfo: false,
+  quiet: false,
+  stats: {
+    version: true,
+    timings: true,
+    modules: false,
+    errorDetails: true,
+    chunkModules: false,
+    colors: true
+  }
+});
+
+server.listen(port, "localhost", function (err) {
+  if (err) {
+    throw err;
+  }
+
+  console.log('☕️  Listening at http://localhost:' + port);
+});
+
---
+++ src/index.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider, combineReducers } from 'fluorine-lib'
+
+import {
+  Route,
+  IndexRoute,
+  Router,
+  browserHistory
+} from 'react-router'
+
+import dispatcher from './dispatcher'
+import reducers from './reducers/index'
+
+import App from './containers/app'
+import Counter from './components/Counter'
+
+const routes = (
+  <Route path='/' component={App}>
+    <IndexRoute component={Counter}/>
+  </Route>
+)
+
+render((
+  <Provider
+    dispatcher={dispatcher}
+    observable={({ reduce }) => combineReducers(reducers)(reduce)}>
+    <Router history={browserHistory} routes={routes}/>
+  </Provider>
+), document.getElementById('root'))
+
